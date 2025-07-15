012ee3ba4e358d8af54ba157cfbceba17f1024f6
soulixai vilasit <107106046+souvls@users.noreply.github.com>
2025-07-15 17:23:09 +0700

    s

--- src/app/cashier/layout.tsx
+++ src/app/cashier/layout.tsx
@@ -1,8 +1,6 @@
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/cashier/Sidebar";
-export const metadata = {
-  title: "SHOP - LATDA",
-};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col h-screen">
